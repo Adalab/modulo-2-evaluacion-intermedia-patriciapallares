862f95b5bd6c7c7c6552554eee04fcacb857097e
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-03-29 11:04:31 +0200

    added Bonus as much as possible

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 'use strict';
 
-console.log('desayuno');
-
-// Constantes
-
-const select = document.querySelector('.js-select');
+// variables
+const selectNum = document.querySelector('.js-select');
 const button = document.querySelector('.js-button');
 const goText = document.querySelector('.js-go-text');
 const emptyText = document.querySelector('.js-empty-text')
 const diceText = document.querySelector('.js-dice');
+
+// BONUS variables: no supe hacerlo, sigo
+let initValue = 50;
+const moneyLeft = document.querySelector('.js-money-left');
+const selectMoney = document.querySelector('.js-user-money');
+
+// BONUS + variables
+const reButton = document.querySelector('.js-re-button');
+const section = document.querySelector('.js-section');
+const winnerIs = document.querySelector('.js-winner-is');
+
+// BONUS pre-eventos: no supe hacerlo, sigo
+moneyLeft.innerHTML = `Saldo: ${initValue}`;
 
 
 // funciones
@@ -17,37 +27,61 @@ function getRandomNumber(max) {
 }
 
 // función para que aparezca un mensaje u otro en función de si el número aleatorio y el de la usuaria son el mismo o no.
-function displayMessage() {
-  
+function displayMessage() { 
   // variable para el número aleatorio
   const pcNum = getRandomNumber(6);
-
-  console.log(pcNum);
-  // variable para el valor seleccionado por la usuaria
-
-  const userNum = select.value;
-  // variable para hacer número el valor seleccionado por la usuaria
+  // variable para el valor seleccionado por la user
+  const userNum = selectNum.value;
+  // variable para hacer número el valor seleccionado por la user
   const realNum = parseInt(userNum); 
-  console.log(realNum);
+
+  // BONUS variable para el valor seleccionado por la user
+  const herChoiceMoney = selectMoney.value;
+  // variable para hacer número el valor seleccionado por la user
+  const realMoney = parseInt(herChoiceMoney); 
 
   if (pcNum === realNum) {
     goText.classList.add('hidden');
-    emptyText.innerHTML = `¡Has ganado el doble de lo apostado :D!`
+    emptyText.innerHTML = `¡Has ganado el doble de lo apostado :D!`;
+    // moneyLeft.innerHTML = `Saldo: ${initValue + realMoney*2}`;
+
   } else if (pcNum !== realNum) {
     goText.classList.add('hidden');
-    emptyText.innerHTML = `¡Has perdido lo apostado D:!`
-  } 
-
+    emptyText.innerHTML = `¡Has perdido lo apostado D:!`;
+    // moneyLeft.innerHTML = `Saldo: ${initValue - realMoney}`;
+  }
   // mostrar el resultado aleatorio  
   diceText.innerHTML = `Dado: ${pcNum}`
 }
 
+// función si moneyLeft === 0 o 200, donde NUM será el valor del saldo total: no supe hacerlo, sigo
+function endOfGame(NUM) {
+  moneyLeft.value = NUM;
+
+  if (NUM === 200){
+    section.classList.add('hidden');
+    winnerIs.innerHTML = `¡¡Eres la ganadora!!`
+    reButton.classList.remove('hidden');
+  } else if ( NUM === 0) {
+    section.classList.add('hidden')
+    winnerIs.innerHTML = `Sorry, ha ganado el cpu :'(`;
+    reButton.classList.remove('hidden');
+  }
+};
+
+function restart() {
+  reButton.classList.add('hidden');
+  winnerIs.classList.add('hidden');
+  section.classList.remove('hidden');
+}
 
 // intento hacer una función manejadora
 function handleClickButton (event) {
   event.preventDefault();
   displayMessage();
+  endOfGame();
 }
 
 // Codigo a ejecutar cuando carga la página
 button.addEventListener('click',handleClickButton);
+reButton.addEventListener('click', restart);
--- readme.md
+++ readme.md
@@ -6,3 +6,12 @@
 - [x] Plantear las condiciones
 - [x] Escribir el "nuevo código" según se cumpla una u otra condición
 
+/// Descripción bonus:
+- [] Hacer que se actualice la cantidad de la usuaria en dependencia de si gana o pierde
+- [] La usuaria gana cuando llega a 200 o a 0, entonces se muestra un botón de reiniciár juego y desaparece el botón de jugar
+- [] Todo vuelve a cero cuando pulsa reiniciar, desaparece este botón y vuelve a aparecer jugar
+- [] Cuando finaliza, muestra quien ha ganado la partida
+
+/// Tareas bonus:
+- [] Declarar nuevas variables en JS
+- [] Añadir funciones necesarias 
