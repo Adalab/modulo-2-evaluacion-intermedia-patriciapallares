75fce51f2085663dbb89775def2ede585a0ce357
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-03-29 09:59:56 +0200

    added functions + click event - working

--- js/main.js
+++ js/main.js
@@ -7,10 +7,39 @@ console.log('desayuno');
 const select = document.querySelector('.js-select');
 const button = document.querySelector('.js-button');
 const goText = document.querySelector('.js-go-text');
-const winText = document.querySelector('.js-win-text');
-const looseText = document.querySelector('.js-loose-text');
+const emptyText = document.querySelector('.js-empty-text')
 
 
-// Funciones
+// funciones
+function getRandomNumber(max) {
+   return Math.ceil(Math.random()*max);
+}
+
+// función para que aparezca un mensaje u otro en función de si el número aleatorio y el de la usuaria son el mismo o no.
+function displayMessage() {
+  // variable para el número aleatorio
+  const pcNum = getRandomNumber(6);
+  console.log(pcNum);
+  // variable para el valor seleccionado por la usuaria
+  const userNum = select.value;
+  // variable para hacer número el valor seleccionado por la usuaria
+  const realNum = parseInt(userNum); 
+  console.log(realNum);
+
+  if (pcNum === realNum) {
+    goText.classList.add('hidden');
+    emptyText.innerHTML = `¡Has ganado el doble de lo apostado :D!`
+  } else if (pcNum !== realNum) {
+    goText.classList.add('hidden');
+    emptyText.innerHTML = `¡Has perdido lo apostado D:!`
+  } 
+}
+
+// intento hacer una función manejadora
+function handleClickButton (event) {
+  event.preventDefault();
+  displayMessage();
+}
 
 // Codigo a ejecutar cuando carga la página
+button.addEventListener('click',handleClickButton);
--- readme.md
+++ readme.md
@@ -1,14 +1,8 @@
-[] La jugadora selecciona el número que quiere apostar
-[] Da click a Jugar
-[] Si se cumple la condición: has ganado
-[] Si no se cumple la condición: has perdido
+/// Tareas:
+- [x] Maquetar Html
+- [x] Declarar variables en JS
+- [x] addEventListener en el botón de jugar
+- [x] Recoger el valor de la usuaria
+- [x] Plantear las condiciones
+- [x] Escribir el "nuevo código" según se cumpla una u otra condición
 
-/// 
-
-Tareas:
-[x] Maquetar Html
-[x] Declarar variables en JS
-[] addEventListener en el botón de jugar
-[] Recoger el valor de la usuaria
-[] Plantear las condiciones
-[] Escribir el "nuevo código" según se cumpla una u otra condición
